3b14506146edc3be143176e431a88dec90712ab2
Debora <mvumodebora@gmail.com>
2026-02-10 08:21:53 +0300

    Add students app, models, migrations, and dashboard structure

--- backend/students/models.py
+++ backend/students/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class StudentProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.user)
--- backend/students/views.py
+++ backend/students/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import StudentProfile
 
-# Create your views here.
+@login_required
+def student_dashboard(request):
+    student = StudentProfile.objects.get(user=request.user)
+
+    context = {
+        "student": student
+    }
+    return render(request, "students_dashboard.html", context)
